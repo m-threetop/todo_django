85021b847879919c812dfbdcb44cbaca3f716b36
mikami <m.mikami.threetop@gmail.com>
2020-05-30 01:43:51 +0900

    wip show list

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Task(models.Model):
+    subject = models.CharField(max_length=128)
+    note = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    deadline_date = models.DateTimeField(null=True, blank=True)
+    finished_date = models.DateTimeField(null=True, blank=True)
+    delete_flag = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.subject
+    
+    def finish_task(self):
+        self.finished_date = timezone.now()
+        self.save()
+    
+    def delete_task(self):
+        self.delete_flag = True
+        self.save()
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
+
+def list(request):
+    tasks = Task.objects.filter(delete_flag=False, finished_date__isnull=True)
+    return render(request, 'todo/list.html', {"tasks": tasks})
